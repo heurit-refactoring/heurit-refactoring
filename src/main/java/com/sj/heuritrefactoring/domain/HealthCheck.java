33912a7e4eadbc7db3f064712104a9b5f1e3d383
JunHo KANG <66174425+JunHo-YH@users.noreply.github.com>
2022-08-26 00:11:14 +0900

    init

--- src/main/java/com/sj/heuritrefactoring/domain/HealthCheck.java
+++ src/main/java/com/sj/heuritrefactoring/domain/HealthCheck.java
@@ -1,4 +1,14 @@
 package com.sj.heuritrefactoring.domain;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheck {
+
+    @GetMapping("/ping")
+    public String healthCheck() {
+
+        return "pong";
+    }
 }
