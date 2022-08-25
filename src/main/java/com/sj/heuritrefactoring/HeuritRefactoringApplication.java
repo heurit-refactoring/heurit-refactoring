e687bb6dfbfa21c563547094e18db01fb255571b
JunHo KANG <66174425+JunHo-YH@users.noreply.github.com>
2022-08-25 22:26:15 +0900

    project init

---
+++ src/main/java/com/sj/heuritrefactoring/HeuritRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.sj.heuritrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeuritRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeuritRefactoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sj/heuritrefactoring/HeuritRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sj.heuritrefactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeuritRefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
